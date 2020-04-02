05423724e9a30607075e89cd2cd14e69b9b8634c
dannyljl <dannylee1997@hotmail.com>
2020-04-02 13:11:17 +0200

    fixed structure

---
+++ Auth/src/main/java/com/jking/login/login/Controllers/TokenController.java
@@ -0,0 +1,21 @@
+package com.jking.login.login.Controllers;
+
+import com.jking.login.login.Repository.IUserRepository;
+import com.jking.login.login.classes.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/token")
+public class TokenController {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @GetMapping
+    public ResponseEntity<String> Login() {
+        return new ResponseEntity<>("Nice Token", HttpStatus.OK);
+    }
+}
---
+++ Auth/src/main/java/com/jking/login/login/LoginApplication.java
@@ -0,0 +1,45 @@
+package com.jking.login.login;
+
+import Server.AccountType;
+import Server.Binding.Secured;
+import com.jking.login.login.Controllers.SpringAuthenFilter;
+import com.jking.login.login.Controllers.SpringAuthorFilter;
+import com.jking.login.login.Repository.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.container.ResourceInfo;
+import javax.ws.rs.core.Context;
+
+@Component
+@SpringBootApplication
+public class LoginApplication {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginApplication.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean<SpringAuthenFilter> authenFilter() {
+        FilterRegistrationBean<SpringAuthenFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new SpringAuthenFilter(userRepository));
+        registrationBean.addUrlPatterns("/token");
+        registrationBean.addUrlPatterns("/admin");
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<SpringAuthorFilter> authorFilter() {
+        FilterRegistrationBean<SpringAuthorFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new SpringAuthorFilter());
+        registrationBean.addUrlPatterns("/admin");
+        return registrationBean;
+    }
+}
