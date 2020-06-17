a812a10d477e72e2c41f6944a33c8721a2ac2beb
dannyljl <dannylee1997@hotmail.com>
2020-06-17 14:33:49 +0200

    gacha

--- guild/src/main/java/com/guild/guild/Controllers/GachaController.java
+++ guild/src/main/java/com/guild/guild/Controllers/GachaController.java
@@ -1,6 +1,5 @@
 package com.guild.guild.Controllers;
 
-import com.guild.guild.classes.Guild;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,9 +10,9 @@ public class GachaController {
 
     //gacha
     @GetMapping("/{maxNumber}")
-    public int getResult(@PathVariable String maxNumber){
+    public String getResult(@PathVariable String maxNumber){
         RestTemplate restTemplate = new RestTemplate();
-        int result = restTemplate.getForObject("https://jking-functions-20200617112822840.azurewebsites.net/api/gacha?max-number=" + maxNumber, Integer.class);
+        String result = restTemplate.getForObject("https://jking-functions-20200617112822840.azurewebsites.net/api/gacha?max-number=" + maxNumber, String.class);
         return result;
     }
 }
