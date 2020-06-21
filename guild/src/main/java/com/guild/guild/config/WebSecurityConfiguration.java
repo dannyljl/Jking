3417f5d5ac3219ff5103a0196ce9c50987965ba7
dannyljl <dannylee1997@hotmail.com>
2020-06-22 00:08:02 +0200

    added authentication on guild server

---
+++ guild/src/main/java/com/guild/guild/config/WebSecurityConfiguration.java
@@ -0,0 +1,8 @@
+package com.guild.guild.config;
+
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfiguration extends GlobalMethodSecurityConfiguration {
+}
