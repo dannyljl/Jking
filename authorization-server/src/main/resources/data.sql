2dba1b60020094f3b5646167d0c9cb3312d4f8b2
dannyljl <dannylee1997@hotmail.com>
2020-06-29 18:25:12 +0200

    removed role inserts

--- authorization-server/src/main/resources/data.sql
+++ authorization-server/src/main/resources/data.sql
@@ -1,7 +1,3 @@
 
 -- The encrypted password is `client-secret`
 INSERT INTO oauth_client_details (client_id, client_secret, scope, authorized_grant_types, authorities, access_token_validity) VALUES ('clientId', '$2a$10$Ych6kpYiY4kPUf8P9wBahexBJk9bS1UYxZDRg8gi71TVHGPkpCEmq', 'read,write', 'password,refresh_token,client_credentials', 'ROLE_CLIENT', 300);
-
-
-INSERT INTO authorities (authority) VALUES ('ROLE_ADMIN');
-INSERT INTO authorities (authority) VALUES ('ROLE_USER');
