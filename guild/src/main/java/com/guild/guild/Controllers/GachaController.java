3b7f1d9c76468683b981e81058e9d1690ba3b3ec
dannyljl <dannylee1997@hotmail.com>
2020-06-29 21:01:06 +0200

    gacha changed to http connection

--- guild/src/main/java/com/guild/guild/Controllers/GachaController.java
+++ guild/src/main/java/com/guild/guild/Controllers/GachaController.java
@@ -14,7 +14,7 @@ public class GachaController {
     //gacha rng
     @GetMapping("/{maxNumber}")
     public String getResult(@PathVariable String maxNumber) throws IOException {
-        String url = "https://jking-functions-20200617112822840.azurewebsites.net/api/gacha?max-number=" + maxNumber;
+        String url = "http://jking-functions-20200617112822840.azurewebsites.net/api/gacha?max-number=" + maxNumber;
         RestTemplate restTemplate = new RestTemplate();
         System.out.println("does it come here?");
         String result = restTemplate.getForObject(url, String.class);
