5c10f50a2e80fd3c147bd141989cd3aced14e0df
dannyljl <dannylee1997@hotmail.com>
2020-06-29 17:37:09 +0200

    maybe gachafix

--- guild/src/main/java/com/guild/guild/Controllers/GachaController.java
+++ guild/src/main/java/com/guild/guild/Controllers/GachaController.java
@@ -13,7 +13,7 @@ public class GachaController {
 
     //gacha rng
     @GetMapping("/{maxNumber}")
-    public String getResult(@PathVariable Long maxNumber) throws IOException {
+    public String getResult(@PathVariable String maxNumber) throws IOException {
         String url = "https://jking-functions-20200617112822840.azurewebsites.net/api/gacha?max-number=" + maxNumber;
         RestTemplate restTemplate = new RestTemplate();
         String urlWhiteListed = "https://jking-functions";
